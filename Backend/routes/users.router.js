7216eb37b8c52947af02de43e702d79df05f8a4f
Jeyran999 <ceyranguliyeva2006@gmail.com>
2026-07-11 16:03:39 +0400

    feat: implement login with JWT authentication

--- Backend/routes/users.router.js
+++ Backend/routes/users.router.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const userController = require("../controllers/users.controller")
-const authValidation = require("../validations/auth.validation")
+const {registerSchema, loginSchema} = require("../validations/auth.validation")
+const registerLimiter = require("../middlewares/rateLimiter.middleware")
+const validate = require("../middlewares/validate.middleware")
+
 
 const userRouter = express.Router()
 
-userRouter.post("/register" , authValidation.register ,userController.register)
+userRouter.post("/register" , registerLimiter , validate(registerSchema) ,userController.register)
+userRouter.post("/login" , validate(loginSchema) ,userController.login)
 
+// Export
 module.exports = userRouter
